e611e19364946a993506c36c0f0e89915ab1274a
pankajmehtaprofessional <pankajmehtaprofessional@gmail.com>
2024-05-31 16:17:00 +0530

    Feature: swagger

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AdminService } from './admin.service';
 import { Admin } from './admin.schema';
 import { ResponseDto } from '../../common/dto/response.dto';
 import { ResponseService } from '../../common/services/response.service';
 
+@ApiTags('Admins')
 @Controller('admins')
 export class AdminController {
   constructor(
@@ -11,6 +13,8 @@ export class AdminController {
     private readonly responseService: ResponseService,
   ) {}
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return the list of all users.' })
   @Get()
   async findAll(): Promise<ResponseDto<Admin[]>> {
     const response = await this.adminService.findAll();
